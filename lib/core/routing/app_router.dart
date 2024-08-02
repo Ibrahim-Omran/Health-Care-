cf2d9147ef7b5c04867cab4d47009ea4915e6c88
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-08-02 17:41:22 +0300

    - added di - added appRegex - integrated with login endPoint - added login Repo&cubit - added login states with validation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:health_care_app/core/di/dependency_injection.dart';
 import 'package:health_care_app/core/routing/routes.dart';
+import 'package:health_care_app/features/home/ui/home_screen.dart';
+import 'package:health_care_app/features/login/logic/login_cubit.dart';
 import 'package:health_care_app/features/login/ui/login_screen.dart';
 import 'package:health_care_app/features/onboarding/onbording_screen.dart';
 
 class AppRouter {
 
-  Route generateRouter(RouteSettings settings){
+  Route generateRouter(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName)
     final arguments = settings.arguments;
 
-    switch(settings.name){
+    switch (settings.name) {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
-            builder: (_)=> const OnboardingScreen()
+            builder: (_) => const OnboardingScreen()
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-            builder: (_)=> const LoginScreen()
+            builder: (_) =>
+                BlocProvider(
+                  create: (context) => getIt<LoginCubit>(),
+                  child: const LoginScreen(),
+                )
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+            builder: (_) => const HomeScreen()
         );
 
       default:
         return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(
-              child: Text('Not Found..! ${settings.name}'),
-            ),
-          ),
+          builder: (_) =>
+              Scaffold(
+                body: Center(
+                  child: Text('Not Found..! ${settings.name}'),
+                ),
+              ),
         );
     }
-
   }
 }
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:health_care_app/core/widgets/app_text_button.dart';
-import 'package:health_care_app/core/widgets/app_text_form_field.dart';
+import 'package:health_care_app/features/login/data/models/login_request_body.dart';
+import 'package:health_care_app/features/login/logic/login_cubit.dart';
 import 'package:health_care_app/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:health_care_app/features/login/ui/widgets/email_and_password.dart';
+import 'package:health_care_app/features/login/ui/widgets/login_bloc_listener.dart';
 import 'package:health_care_app/features/login/ui/widgets/terms_and_conditions_text.dart';
 
 import '../../../core/helper/spacing.dart';
 import '../../../core/theme/styles.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final formKey = GlobalKey<FormState>();
-  bool isObscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -39,52 +35,31 @@ class _LoginScreenState extends State<LoginScreen> {
                   style: TextStyles.font14GrayRegular,
                 ),
                 verticalSpace(36),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      const AppTextFormField(
-                        hintText: 'Email',
+                Column(
+                  children: [
+                    const EmailAndPassword(),
+                    verticalSpace(24),
+                    Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyles.font13BlueRegular,
                       ),
-                      verticalSpace(18),
-                      AppTextFormField(
-                        hintText: 'Password',
-                        isObscureText: isObscureText,
-                        suffixIcon: GestureDetector(
-                          onTap: (){
-                            setState(() {
-                              isObscureText = !isObscureText ;
-                            });
-                          },
-                          child: Icon(
-                            isObscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                        ),
-                      ),
-                      verticalSpace(24),
-                      Align(
-                        alignment: AlignmentDirectional.centerEnd,
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyles.font13BlueRegular,
-                        ),
-                      ),
-                      verticalSpace(40),
-
-                      AppTextButton(
-                          buttonText: 'Login',
-                          textStyle: TextStyles.font16WhiteSemiBold,
-                          onPressed: (){},
-                      ),
-                      verticalSpace(16),
-                      const TermsAndConditionsText(),
-                      verticalSpace(60),
-                      const AlreadyHaveAccountText(),
-
-                    ],
-                  ),
+                    ),
+                    verticalSpace(40),
+                    AppTextButton(
+                      buttonText: 'Login',
+                      textStyle: TextStyles.font16WhiteSemiBold,
+                      onPressed: () {
+                        validateThenDoLogin(context);
+                      },
+                    ),
+                    verticalSpace(16),
+                    const TermsAndConditionsText(),
+                    verticalSpace(60),
+                    const AlreadyHaveAccountText(),
+                    const LoginBlocListener(),
+                  ],
                 ),
               ],
             ),
@@ -92,5 +67,16 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+  void validateThenDoLogin(BuildContext context) {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().emitLoginState(
+            LoginRequestBody(
+              email: context.read<LoginCubit>().emailController.text,
+              password: context.read<LoginCubit>().passwordController.text,
+            ),
+          );
+    }
   }
 }
